faf1fb1065e5b1812b218824812022fa933522c4
Zazas <bran12chembah@outlook.com>
2021-03-08 20:59:59 -0500

    initialized project

---
+++ src/components/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>DevCamp React Starter</h1>
+        <h2>React Redux</h2>
+        <h1>The Best way to send money </h1>
+      </div>
+    );
+  }
+}
